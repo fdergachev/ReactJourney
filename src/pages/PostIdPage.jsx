29c4b2753222e6d44718bd2e06161a588308dc57
Fedir Derhachov <fdeordergachev@gmail.com>
2023-07-26 19:31:28 +0200

    Additions Added functional on PostById page Added content on PostById page Added comments on PBI page

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/usefetching.js'
+import PostService from '../API/postservice.js'
+import Loader from '../components/UI/loader/loader.jsx';
+
 
 const PostIdPage = () => {
+   const params = useParams();
+   const [post, setPost] = useState({})
+   const [comments, setComments] = useState([])
+   const [fetchPostsById, isLoading, error] = useFetching(async (id) => {
+      const response = await PostService.getById(id)
+      setPost(response.data)
+   });
+   const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+      const response = await PostService.getCommentsByPostId(id)
+      setComments(response.data)
+   });
+
+
+   useEffect(() => {
+      fetchPostsById(params.id);
+      fetchComments(params.id);
+   }, [])
+
    return (
       <div>
-         <h1>post</h1>
+         <h1>Вы открыли страницу поста с ID - {params.id}</h1>
+         {isLoading
+            ? <Loader />
+            : <div className="">{post.id}. {post.title}</div>
+         }
+         <h1>Комментарии</h1>
+         {isComLoading
+            ? <Loader />
+            : <div className="">
+               {comments.map(comm =>
+                  <div style={{ marginTop: 15 }}>
+                     <h5>{comm.email}</h5>
+                     <div className="">{comm.body}</div>
+                  </div>
+               )}
+            </div>
+         }
       </div>
    );
 };
